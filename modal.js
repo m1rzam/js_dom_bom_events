b529e97890b45f4f8d88673f89086f66aadcaf6a
Andrew Kryuchkov <andrew.kryuchkov@gmail.com>
2018-12-03 01:09:08 +0200

    pravka 1

--- modal.js
+++ modal.js
@@ -7,8 +7,8 @@ button.onclick = function () {
 span.onclick = function () {
     modal.style.display = "none";
 }
-window.onclick = function (event) {
-    if(event.target == modal) {
-        modal.style.display = "none";
-    }
-}
+// window.onclick = function (event) {
+//     if(event.target == modal) {
+//         modal.style.display = "none";
+//     }
+// }
